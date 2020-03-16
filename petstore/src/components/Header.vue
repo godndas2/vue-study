d0a178fe3af80484c58ae641a6a8496f67e76d9c
halfdev <gusgodnd@naver.com>
2020-03-17 00:20:07 +0900

    #1 vue-cli somthing wrong (npm run serve -> white page)

---
+++ petstore/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+<header>
+  <div class="navbar navbar-default">
+    <div class="navbar-header">
+      <h1><router-link :to="{name: 'iMain'}">{{ sitename }}</router-link></h1> <!-- iMain path로 연결 -->
+    </div>
+    <div class="nav navbar-nav navbar-right cart">
+      <!-- <button type="button" class="btn btn-default btn-lg" v-on:click="showCheckout">
+        <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> Check out
+      </button> -->
+      <router-link
+          active-class="active"
+          tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
+        <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> Check out
+      </router-link>
+    </div>
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'my-header',
+  data() {
+    return {
+      sitename: "Vue Pet-Shop"
+    }
+  },
+  props: ['cartItemCount'],
+  methods: {
+    // showCheckout() {
+    //   this.$router.push({name: 'Form'}); Form Path로 이동
+    // }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+.router-link-exact-active {
+  /* color: blue; */
+  color: black;
+}
+</style>
---
+++ petstore/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Form from '@/components/Form'
+import Main from '@/components/Main'
+// import Product from '@/components/Product'
+// import EditProduct from '@/components/EditProduct'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history', // hash를 사용하는 경로의 history mode
+  routes: [
+    {
+      path: '/',
+      name: 'iMain',
+      component: Main,
+      props: true
+    },
+    {
+      path: '/form',
+      name: 'Form',
+      component: Form,
+      props: true
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
