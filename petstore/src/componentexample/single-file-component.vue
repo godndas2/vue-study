37e9cb2337adbac384d59d435c57f2ca60584fb8
halfdev <gusgodnd@naver.com>
2020-03-17 00:37:07 +0900

    #2 resolve PATH (but, white page)

---
+++ petstore/src/componentexample/single-file-component.vue
@@ -0,0 +1,21 @@
+<!-- 대형 Application에 용이하다. -->
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
